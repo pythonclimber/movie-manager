18302ebb74877321508e2cc10793c854e75379f3
Aaron Smith <aosmith@thebancorp.com>
2018-03-02 09:52:40 -0600

    Refining search page

--- movie-manager/app/pages/search-page/search-view-model.ts
+++ movie-manager/app/pages/search-page/search-view-model.ts
@@ -1,5 +1,16 @@
 import { Observable } from "ui/frame";
 
 export class SearchViewModel extends Observable {
+    private _searchText: string;
 
+    get searchText(): string {
+        return this._searchText;
+    }
+
+    set searchText(value: string) {
+        if (value !== this._searchText) {
+            this._searchText = value;
+            this.notifyPropertyChange('searchText', value);
+        }
+    }
 }
