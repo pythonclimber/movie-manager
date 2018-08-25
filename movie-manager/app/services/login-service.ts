b41123b4f3848367b181ea91b89153312b3bba99
Aaron Smith <asmitty92@gmail.com>
2018-08-25 18:10:13 -0500

    cred issue #3

--- movie-manager/app/services/login-service.ts
+++ movie-manager/app/services/login-service.ts
@@ -14,7 +14,7 @@ export class LoginService extends BaseService {
         try {
             this.credentials = this.GetAppSetting<SavedCredentials[]>(CREDENTIALS_KEY);
         } catch (error) {
-            this.credentials = new Array<SavedCredentials>();;
+            this.credentials = new Array<SavedCredentials>();
         }
     }
 
